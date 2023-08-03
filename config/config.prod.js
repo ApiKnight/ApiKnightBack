6413a4b2b3df046658cebb73c44fd72969e87398
1379255913 <1379255913@qq.com>
2023-08-03 22:09:22 +0800

    fix: 修复数据库错误

--- config/config.prod.js
+++ config/config.prod.js
@@ -10,16 +10,16 @@ module.exports = app => {
   const config = {}
 
   config.sequelize = {
-    username: 'root',
-    password: '',
-    database: 'template_node_egg',
+    // 连接用户
+    username: 'node_egg',
+    // 连接密码
+    password: 'JHw2xb4BMfJkD3rZ',
+    // 连接的数据库，可根据需要改成已有的数据库
+    database: 'node_egg',
+    // 连接地址
     host: '127.0.0.1',
+    // 数据库类型
     dialect: 'mysql'
-  }
-
-  // 生产环境跨域配置
-  config.cors = {
-    origin: ''
   }
 
   // 自定义日志路径
