6f1e37380859edf9f258d72ac04d0901a84c6c8e
LeeZ1Q <466944567@qq.com>
2023-08-28 17:25:31 +0800

    fix:whiteUrl for mock

--- config/config.default.js
+++ config/config.default.js
@@ -124,7 +124,8 @@ module.exports = app => {
       `${apiPrefix}/v1/user/register`,
       `${apiPrefix}/v1/user/checkExist`,
       `${apiPrefix}/v1/project/querysummary`,
-      `${apiPrefix}/v1/mock/real`
+      `${apiPrefix}/v1/mock/real`,
+      `${apiPrefix}/v1/mock/:id/:url*`
     ]
   }
 
